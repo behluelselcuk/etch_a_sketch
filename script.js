7ce77664db46b09bea9a12ab5ab8b9cac3d9490a
Behlül Samil Selcuk <157593831+behluelselcuk@users.noreply.github.com>
2025-02-03 14:41:43 +0100

    Füge Button für Anzahl der Quadrate im Raster zu

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 'use strict';
 
-// innerhalb des rasters 16x16er divs erstellen, sodass das Raster gefüllt mit rechteckigen divs gleicher Größe ist
+// Elemente deklarieren
 let raster = document.querySelector('.raster');
+let numberBtn = document.querySelector('#numberBtn');
 
-// erstens:
-// divs erstellen
+
+// divs erstellen & gleichmäßig im raster einordnen
 for (let i = 0; i < 256; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     cell.style.width = '50px';
     cell.style.height = '50px';
-    cell.style.border = '1px solid black'; // Optional: Rahmen hinzufügen
     raster.appendChild(cell);
 }
 
-// zweitens:
-// divs gleichmäßig im raster einordnen
+// numberBtn-Funktion erstellen
+numberBtn.addEventListener('click', () => {
+    let cellMultiplier;
+    do {
+        cellMultiplier = parseInt(prompt('Wie viele Quadrate pro Seite? (max. 100)', 10))
+    } while (isNaN(cellMultiplier) || cellMultiplier > 100);
+
+    let sumCells = cellMultiplier**2;
+    raster.innerHTML = '';
+
+    for (let i = 0; i < sumCells; i++) {
+        const newCell = document.createElement('div');
+        newCell.classList.add('cell');
+
+        const size = 800 / cellMultiplier;
+        newCell.style.width = `${size}px`;
+        newCell.style.height = `${size}px`;
+        newCell.dataset.brightness = 100;
+
+        raster.appendChild(newCell);
+    }
+})
+
